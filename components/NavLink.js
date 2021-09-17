f1abf33650172176f488e7c97dd920b5bbfb9492
gylim0604 <gylim0604@hotmail.com>
2021-09-17 21:21:02 +0800

    added some simple transitions for the links

--- components/NavLink.js
+++ components/NavLink.js
@@ -6,13 +6,22 @@ const NavLink = (props) => {
             pos='relative'
             _hover={{
                 _after: {
-                    content: '""',
-                    position: 'absolute',
-                    bottom: '-7.5px',
-                    right: '25%',
-                    left: '25%',
-                    borderBottom: '2px solid white',
+                    transform: 'scaleX(1)',
+                    transformOrigin: 'left',
+                    transitionDelay: '0ms',
                 },
+            }}
+            _after={{
+                content: '""',
+                position: 'absolute',
+                bottom: '-7.5px',
+                right: '25%',
+                left: '25%',
+                borderBottom: '2px solid white',
+                transform: 'scaleX(0)',
+                transformOrigin: 'right',
+                transition: 'transform 200ms ease-in-out',
+                transitionDelay: '200ms',
             }}
         >
             {props.children}
